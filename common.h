2f5935fce5ba9045a95b7c4718287907a432eb64
zys <2741471872@qq.com>
2026-03-11 20:50:00 +0800

    增加成员名字

--- common.h
+++ common.h
@@ -2,9 +2,11 @@
 #define COMMON_H
 
 #define MAX_DEPARTMENT_LENGTH 1000
+#define MAX_NAME_LENGTH 1000
 struct Staff
 {
     char department[MAX_DEPARTMENT_LENGTH];
+    char name[MAX_NAME_LENGTH];
     int holidayTime;
 };
 
