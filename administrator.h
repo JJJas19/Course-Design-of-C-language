fdfafb005137c9114177695d9474c41db627f2a0
zys <2741471872@qq.com>
2026-03-11 19:39:57 +0800

    新增了基础头文件common.h

--- administrator.h
+++ administrator.h
@@ -1,11 +1,6 @@
 #ifndef ADMINISTRATOR_H
 #define ADMINISTRATOR_H
 
-#define MAX_DEPARTMENT_LENGTH 1000
-struct Staff
-{
-    char department[MAX_DEPARTMENT_LENGTH];
-    int holidayTime;
-};
+#include "common.h"
 
 #endif 
---
+++ common.h
@@ -0,0 +1,11 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define MAX_DEPARTMENT_LENGTH 1000
+struct Staff
+{
+    char department[MAX_DEPARTMENT_LENGTH];
+    int holidayTime;
+};
+
+#endif 
