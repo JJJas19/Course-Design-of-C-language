586483f6f557c5b1b6b84e168a74961df9986cff
zys <2741471872@qq.com>
2026-03-11 19:29:07 +0800

    定义头文件和完成部分管理员角色代码

---
+++ administrator.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "administrator.h"
+
+typedef struct Staff Staff;
+typedef struct Node
+{
+    Staff data;
+    Staff* next;
+} Node;
+
+Node *head, *tail;
+
+Node *createNode(Staff data)
+{
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("CreateNode Error\n");
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void init()
+{
+    Staff data = {0, NULL};
+    head = createNode(data);
+    tail = createNode(data);
+    head->next = tail;
+}
+
+void addStaff(const char* department, int holidayTime)
+{
+    Staff data;
+    data.holidayTime = holidayTime;
+    int size = strlen(department) + 1;
+    if (size > MAX_DEPARTMENT_LENGTH) {
+        printf("Exceeding the maximum length limit.");
+        return;
+    }
+    memcpy(data.department, department, size); 
+    Node *newNode = createNode(data);
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void removeStaff(const char* department)
+{
+    Node *pointer = head;
+    while (pointer->next != NULL)
+    {
+        Node *preNode = pointer;
+        pointer = pointer->next;
+        if (strcmp(pointer->data.department, department) == 0) {
+            preNode->next = pointer->next;
+            if (pointer == tail) {
+                tail = preNode;
+            }
+            free(pointer);
+            printf("Delete Successfully.");
+            return;
+        }
+    }
+    printf("There is no such name.");
+}
+
+void modifyStaff()
+{
+    
+}
+
---
+++ administrator.h
@@ -0,0 +1,11 @@
+#ifndef ADMINISTRATOR_H
+#define ADMINISTRATOR_H
+
+#define MAX_DEPARTMENT_LENGTH 1000
+struct Staff
+{
+    char department[MAX_DEPARTMENT_LENGTH];
+    int holidayTime;
+};
+
+#endif 
