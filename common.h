723ed004e46a9dde7bc0fd792fd52c813d07c113
zys <2741471872@qq.com>
2026-03-13 14:35:43 +0800

    修改了类型定义

--- common.h
+++ common.h
@@ -5,13 +5,58 @@
 #include <windows.h>
 #include "clear_screen.h"
 
-#define MAX_DEPARTMENT_LENGTH 1000
+#define MAX_DEPARTMENT_NAME_LENGTH 1000
 #define MAX_NAME_LENGTH 1000
-struct Staff
+
+// 员工信息
+typedef struct 
 {
-    char department[MAX_DEPARTMENT_LENGTH];
+    int department_id;
     char name[MAX_NAME_LENGTH];
     int holidayTime;
-};
+} Staff;
+
+typedef struct
+{
+    int department_id;
+    char name[MAX_DEPARTMENT_NAME_LENGTH];
+} Department;
+
+// 时间结构体
+typedef struct 
+{
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+} Time;
+
+// 假期类型
+typedef enum
+{
+    annual = 1, // 年假
+    sick, // 病假
+    personal, // 事假
+} HolidayType;
+
+// 假期状态
+typedef enum
+{
+    pending = 1, // 待审批
+    approved, // 通过
+    rejected // 拒绝
+} HolidayStatus;
+
+// 角色类型
+typedef enum
+{
+    administrator = 1,
+    manager,
+    staff,
+    humanresource
+} RoleType;
+
+
 
 #endif
