49b7a3596946ab85f984d92adb77bff3b0b8c59c
ji_tai <3433175957@qq.com>
2026-03-13 13:59:22 +0800

    新增菜单源文件

---
+++ hr_specialist.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+#include <windows.h>
+#include<string.h>
+#include<stdlib.h>
+
---
+++ menu.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void menu()
+{
+printf("**********欢迎使用考勤管理系统**********\n");
+printf("**************************************\n");
+printf("**********1.登录             **********\n");
+printf("**********2.退出             **********\n");
+printf("***************************************\n");
+    int log;
+    scanf("%d",&log);
+    if(log == 1){
+        for(int i = 0;i < 3;i++){
+            printf("请输入账号与密码\n");
+            char account[21];
+            printf("账号：");
+            scanf("%s",account);
+            printf("%s\n",account);
+            int password[21];
+            printf("密码：");
+            char temp;
+            do{                        
+            temp = getchar();//临时存储密码的每一位
+            password[i] = temp;
+            }while(temp!="\n");
+            login(account,password);
+        }
+    }
+}
+void login(char account[],int password[])
+{
+
+
+}
+
+
+
+
+
