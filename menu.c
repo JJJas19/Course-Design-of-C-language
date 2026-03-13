9d9d5917ed12b694eef23faebe4c1e95839f6e44
ji_tai <3433175957@qq.com>
2026-03-13 14:14:24 +0800

    menu测试文件

--- menu.c
+++ menu.c
@@ -14,24 +14,24 @@ printf("***************************************\n");
     if(log == 1){
         for(int i = 0;i < 3;i++){
             printf("请输入账号与密码\n");
-            char account[21];
+            char account[21];//存储账号
             printf("账号：");
             scanf("%s",account);
             printf("%s\n",account);
-            int password[21];
+            int password[21];//存储密码
             printf("密码：");
             char temp;
             do{                        
             temp = getchar();//临时存储密码的每一位
-            password[i] = temp;
-            }while(temp!="\n");
+            password[i] = temp-'0';
+            }while(temp != "\n");
             login(account,password);
         }
     }
 }
 void login(char account[],int password[])
 {
-
+    
 
 }
 
---
+++ menu.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <stdio.h> 
+void menu();
+void login(char account[],int password[]);
