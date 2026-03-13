6acc4125f220206e5e5c8b06881b6fcfef0440ed
DELL <DELL@DESKTOP-UT5SREF>
2026-03-13 14:59:24 +0800

    新增普通职员逻辑

---
+++ employee.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "employee.h"
+
+bool Clock(Employee* employee) {
+	while (true) {
+		printf("要进行打卡吗？(请输入数字)\n");
+		printf("1.是\t2.否");
+		int clocking;
+		scanf("%d", &clocking);
+
+		while (clocking != 1 && clocking != 2) {
+			printf("输入错误，请重新输入!/n");
+			printf("要进行打卡吗？(请输入数字)\n");
+			printf("1.是\t2.否");
+			scanf("%d", &clocking);
+		}
+
+		if (clocking == 1) {
+
+		}
+		else if (clocking == 2) {
+			return false;
+		}
+
+		return false;
+	}
+}
+
+void EmployLogic(Employee* employee) {
+	printf("职员姓名: %s\n", employee->employeeName);
+	printf("职员ID:%d\n", employee->employeeID);
+	Clock(employee);
+}
