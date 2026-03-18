18e486ed4435a0f791d655ff94c36d6174b4c92a
zys <2741471872@qq.com>
2026-03-18 16:34:34 +0800

    修改了头文件

--- administrator.h
+++ administrator.h
@@ -7,4 +7,10 @@ int addDepartmentNode(int departmentID, const char* departmentName);
 int removeDepartmentNode(int index);
 int modifyDepartmentNode(int index);
 void freeDepartmentNode();
+void initEmployeeList();
+int addEmployeeNode(int employeeID, int departmentID, const char* employeeName);
+int setEmployeeDepartment(int employeeID, int departmentID);
+int setHolidayTime(int holidayID, int minimumTime, int maximumTime);
+void freeNode();
+
 #endif 
