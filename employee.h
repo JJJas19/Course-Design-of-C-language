d27f34a19198fcb7a34f019d4e461ad1dde05961
DELL <DELL@DESKTOP-UT5SREF>
2026-03-20 17:08:27 +0800

    添加了新选项，选择上班打卡还是下班打卡

--- employee.h
+++ employee.h
@@ -4,95 +4,124 @@
 // 定义Clock函数，用于处理员工打卡操作，参数为指向Employee结构体的指针
 bool Clock(Employee* employee) {
     while (true) {
-        // 提示用户是否要进行打卡
-        printf("要进行打卡吗？(请输入数字)\n");
-        printf("1.是\t2.否\n");
-        int clocking; // 声明一个整数变量clocking用于存储用户的输入
-        scanf("%d", &clocking); // 读取用户输入的数字
-        int c;
-        // 清空输入缓冲区，直到遇到换行符或文件结束符
-        while ((c = getchar()) != '\n' && c != EOF);
-
-        // 如果用户输入的数字不是1或2，则提示输入错误，并要求重新输入
-        while (clocking != 1 && clocking != 2) {
+        printf("请选择打卡类型:\n");
+        printf("0.返回\t1.上班\t2.下班\n");
+        int clockingType;
+        scanf("%d", &clockingType);
+        if (clockingType == 0) {
+            return;
+        }
+        if (clockingType != 1 && clockingType != 2) {
             printf("输入错误，请重新输入!\n");
+            continue;
+        }
+        while (true) {
+            // 提示用户是否要进行打卡
             printf("要进行打卡吗？(请输入数字)\n");
             printf("1.是\t2.否\n");
-            scanf("%d", &clocking);
+            int clocking; // 声明一个整数变量clocking用于存储用户的输入
+            scanf("%d", &clocking); // 读取用户输入的数字
+            int c;
+            // 清空输入缓冲区，直到遇到换行符或文件结束符
             while ((c = getchar()) != '\n' && c != EOF);
-        }
 
-        // 如果用户选择进行打卡（输入1）
-        while (clocking == 1) {
-            // 提示用户输入打卡时间，并告知输入0可以返回上一级菜单
-            printf("请输入打卡时间:(输入0返回)\n");
-            char input[1000]; // 声明一个字符数组input用于存储用户输入的时间字符串
-            fgets(input, sizeof(input), stdin); // 读取用户输入的时间字符串
-            int len = strlen(input); // 获取输入字符串的长度
-            // 如果用户输入的是"0"（仅包含一个字符'0'和换行符'\n'），则将clocking设置为0并退出循环
-            if (len == 2 && input[0] == '0') {
-                clocking = 0;
-                break;
+            // 如果用户输入的数字不是1或2，则提示输入错误，并要求重新输入
+            while (clocking != 1 && clocking != 2) {
+                printf("输入错误，请重新输入!\n");
+                printf("要进行打卡吗？(请输入数字)\n");
+                printf("1.是\t2.否\n");
+                scanf("%d", &clocking);
+                while ((c = getchar()) != '\n' && c != EOF);
             }
-            // 如果用户输入的是格式为"HH:MM"的时间字符串
-            else if (len == 6 && input[2] == ':') {
-                // 将小时和分钟从字符串中解析为整数
-                int hour = (input[0] - '0') * 10 + (input[1] - '0');
-                if (hour == 24) {
-                    hour -= 24; // 如果小时为24，则将其调整为0，表示午夜
+
+            // 如果用户选择进行打卡（输入1）
+            while (clocking == 1) {
+                // 提示用户输入打卡时间，并告知输入0可以返回上一级菜单
+                printf("请输入打卡时间:(输入0返回)\n");
+                char input[1000]; // 声明一个字符数组input用于存储用户输入的时间字符串
+                fgets(input, sizeof(input), stdin); // 读取用户输入的时间字符串
+                int len = strlen(input); // 获取输入字符串的长度
+                // 如果用户输入的是"0"（仅包含一个字符'0'和换行符'\n'），则将clocking设置为0并退出循环
+                if (len == 2 && input[0] == '0') {
+                    clocking = 0;
+                    break;
                 }
-                else if (hour > 24) {
-                    printf("输入错误，请重新输入!\n"); // 如果小时大于24，则提示输入错误，并要求重新输入
-                    continue;
-                }
-                int minute = (input[3] - '0') * 10 + (input[4] - '0'); // 计算分钟
-                if (minute >= 60) {
-                    printf("输入错误，请重新输入!\n"); // 如果分钟大于等于60，则提示输入错误，并要求重新输入
-                    continue;
-                }
-                // 为clockNotingData分配内存空间
-                employee->clockNotingData = (ClockNoting*)malloc(sizeof(ClockNoting));
-                // 设置打卡时间
-                employee->clockNotingData->clockInTime.hour = hour;
-                employee->clockNotingData->clockInTime.minute = minute;
-                printf("打卡成功！\n"); // 打印打卡成功提示
-                printf("打卡时间为:"); // 打印打卡时间
-                // 根据小时和分钟的值，格式化输出时间，确保小时和分钟都是两位数
-                if (hour < 10) {
-                    printf("0%d:", hour);
+                // 如果用户输入的是格式为"HH:MM"的时间字符串
+                else if (len == 6 && input[2] == ':') {
+                    // 将小时和分钟从字符串中解析为整数
+                    int hour = (input[0] - '0') * 10 + (input[1] - '0');
+                    if (hour == 24) {
+                        hour -= 24; // 如果小时为24，则将其调整为0，表示午夜
+                    }
+                    else if (hour > 24) {
+                        printf("输入错误，请重新输入!\n"); // 如果小时大于24，则提示输入错误，并要求重新输入
+                        continue;
+                    }
+                    int minute = (input[3] - '0') * 10 + (input[4] - '0'); // 计算分钟
+                    if (minute >= 60) {
+                        printf("输入错误，请重新输入!\n"); // 如果分钟大于等于60，则提示输入错误，并要求重新输入
+                        continue;
+                    }
+                    // 为clockNotingData分配内存空间
+                    employee->clockNotingData = (ClockNoting*)malloc(sizeof(ClockNoting));
+                    // 设置打卡时间
+                    if (clockingType == 1) {
+                        if (employee->clockNotingData->clockInTime.isClocking == 1) {
+                            printf("请不要重复打卡!\n");
+                            continue;
+                        }
+                        employee->clockNotingData->clockInTime.isClocking = 1;
+                        employee->clockNotingData->clockInTime.hour = hour;
+                        employee->clockNotingData->clockInTime.minute = minute;
+                    }
+                    else if (clockingType == 2) {
+                        if (employee->clockNotingData->clockOutTime.isClocking == 1) {
+                            printf("请不要重复打卡!\n");
+                            continue;
+                        }
+                        employee->clockNotingData->clockOutTime.isClocking = 1;
+                        employee->clockNotingData->clockOutTime.hour = hour;
+                        employee->clockNotingData->clockOutTime.minute = minute;
+                    }
+                    printf("打卡成功！\n"); // 打印打卡成功提示
+                    printf("打卡时间为:"); // 打印打卡时间
+                    // 根据小时和分钟的值，格式化输出时间，确保小时和分钟都是两位数
+                    if (hour < 10) {
+                        printf("0%d:", hour);
+                    }
+                    else {
+                        printf("%d:", hour);
+                    }
+                    if (minute < 10) {
+                        printf("0%d\n", minute);
+                    }
+                    else {
+                        printf("%d\n", minute);
+                    }
+                    return true; // 打卡成功，返回true
                 }
                 else {
-                    printf("%d:", hour);
+                    printf("输入错误，请重新输入!\n"); // 如果输入格式不正确，则提示输入错误，并要求重新输入
+                    continue;
                 }
-                if (minute < 10) {
-                    printf("0%d\n", minute);
-                }
-                else {
-                    printf("%d\n", minute);
-                }
-                return true; // 打卡成功，返回true
             }
-            else {
-                printf("输入错误，请重新输入!\n"); // 如果输入格式不正确，则提示输入错误，并要求重新输入
-                continue;
+
+            // 如果用户选择取消打卡（输入2）
+            if (clocking == 2) {
+                printf("取消打卡。\n"); // 打印取消打卡提示
+                return false; // 返回false，表示未打卡
             }
-        }
 
-        // 如果用户选择取消打卡（输入2）
-        if (clocking == 2) {
-            printf("取消打卡。\n"); // 打印取消打卡提示
-            return false; // 返回false，表示未打卡
-        }
+            // 如果用户选择返回（输入0）
+            if (clocking == 0) {
+                printf("返回到上一个界面。\n"); // 打印返回提示
+                continue; // 继续下一次循环
+            }
 
-        // 如果用户选择返回（输入0）
-        if (clocking == 0) {
-            printf("返回到上一个界面。\n"); // 打印返回提示
-            continue; // 继续下一次循环
+            return false; // 这里实际上是多余的，因为在clocking == 1和2时已经返回
         }
-
-        return false; // 这里实际上是多余的，因为在clocking == 1和2时已经返回
+        return false; // 这里也是多余的，因为在while(true)循环中不会执行到这里
     }
-    return false; // 这里也是多余的，因为在while(true)循环中不会执行到这里
 }
 
 //某日的打卡情况
